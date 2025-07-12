cf9c233fed6bfa3713a0bfd472b77966a021e562
LuisColoma <92355088+LuisColoma@users.noreply.github.com>
2025-07-12 14:50:20 -0600

    iOs
    iOs

---
+++ app/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_unity_widget/FlutterUnityWidgetPlugin.h>)
+#import <flutter_unity_widget/FlutterUnityWidgetPlugin.h>
+#else
+@import flutter_unity_widget;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterUnityWidgetPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterUnityWidgetPlugin"]];
+}
+
+@end
